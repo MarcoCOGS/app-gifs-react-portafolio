5c4334825f540bcb4b493e75ef8554fb43f433fe
Marco Gallegos <marcogs1202@gmail.com>
2022-06-29 00:17:32 -0500

    octavo docs

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -11,7 +11,7 @@ export const getGifs = async(category)=>{
             url: img.images.downsized_medium.url
         }))
     
-        console.log(data)
+        //console.log(data)
     
     return gifs
 }
